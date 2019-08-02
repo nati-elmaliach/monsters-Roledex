64bf429a5ca091edaaf39da7f719b133f4178963
nati-elmaliach <natielmaliach3197@gmail.com>
2019-08-02 15:07:36 +0300

    Updates

---
+++ precache-manifest.0738647263ecfdb8c5c6a12b017de331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe70fd00706d4b19b5cf5391c49a184d",
+    "url": "/monsters-Roledex/index.html"
+  },
+  {
+    "revision": "9bf2e81c7661d54f9b9e",
+    "url": "/monsters-Roledex/static/css/main.9fb4c1c7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-Roledex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9bf2e81c7661d54f9b9e",
+    "url": "/monsters-Roledex/static/js/main.97b95f78.chunk.js"
+  },
+  {
+    "revision": "f7ddb97bdf0432def1f4",
+    "url": "/monsters-Roledex/static/js/runtime~main.f2c90b16.js"
+  }
+]);
